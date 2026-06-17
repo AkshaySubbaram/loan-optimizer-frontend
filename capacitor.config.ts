f3ceccde75f9a33175b3818ec134559a3f760e03
AkshaySubbaram <subbaramakshay@gmail.com>
2026-06-17 20:41:36 +0530

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ayrash.mobile',
+  appName: 'AyRashMobile',
+  webDir: 'dist/loan-frontend/browser'
+};
+
+export default config;
